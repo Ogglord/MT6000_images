ffed0c5f59765fba90ca3223931591908c6bfb80
Ubuntu <ogge@sandbox.cignl.cc>
2025-01-25 20:02:27 +0000

    adding old builds

---
+++ 2025-01-15_r28709-1657273031-ogglord_next-r4.4.7.mtk/packages/aarch64_cortex-a53/routing/packages.adb
@@ -0,0 +1,4 @@
+ADBdURMhAž¬Vª¥Ö@A-"*ènÒ˜¬Eû‡HEhj‘*e²3Ùº»vf“æbsPoU½±=xÑ
+U¼‚Ô“B‹–R‹?=
+Ú“ Šú6±¢ß¾™7ï{ß7³ÓÓßkpŽ¿ÖPu<ÓÐjcŽ(S9:ey8jk2‘L)Ýo‹»R,íÄÊÛÑK‹æ¯Êû;—uFjå˜KL°+|†¹§®x‘a—xÄr™§p ¸ÃUç…{#kÀ÷´¡÷Ð"ñLFœVÕfDræc—)[Ð*Ã^ž[Ï=ƒ´Õ	-B-Å_9eKá2L(eá+!YeS–,+ä‰KÌqœµAª@Ì1b1œ¸C%.2_×Î‹E+›á@‚	¥lãT L|Še3…v¨üÏ'	=Tí	Ïáû×\óHÎ#7â›vGjÔ¾bã:i?;u&£'„Ä2Nöát6–gŒÊ¸/ÀgÅk~p» 2x”ù7mÜ%KÝ2L–þæÊŽï²•*ÈÎx¼T*¢À<ÝeÒ6„oÅëž3Ã_ÿ-‚*°ð$ÊmÕç ¡Jr¾
+§Å`]ñG­nƒw²°¹æ¥ºŸ‚ý0vAß>Üò¦ïÎÏxôÁ‡É™ÊÚ–á_Í“™[#Kß­/î¾á@Ý@=B•@¸]÷ÁÇeÀ`°@#¡5Í- ¦BMû@£ê núSòæa<P7ª{>dG’OÎf3õOKu%ú5NN4¹vôÓ£Ï¹{™îöÍ/g—§&¦æ´»óÞ¾ô¢ñb´Ýt÷Î~mú¾Öû~ÿŠË£¯æ[IËzØÿ7
---
+++ files/etc/profile.d/zsh.sh
@@ -0,0 +1,6 @@
+# by ogglord
+if [[ $- == *i* ]]; then
+    if [ -x /usr/bin/zsh ]; then
+    	exec /usr/bin/zsh
+    fi
+fi
