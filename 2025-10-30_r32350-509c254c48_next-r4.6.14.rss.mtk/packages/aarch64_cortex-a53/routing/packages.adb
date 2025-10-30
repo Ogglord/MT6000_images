6227dce3038b9bb29a221699c40b2d29ff885f61
Ogglord <oag@proton.me>
2025-10-30 21:20:26 +0000

    4.6.14.mtk dropbear

---
+++ 2025-10-30_r32350-509c254c48_next-r4.6.14.rss.mtk/packages/aarch64_cortex-a53/routing/packages.adb
@@ -0,0 +1,4 @@
+ADBdURMhAÞÄÖ*J‰ÒR•‡T,ènÒÚæPúcÐÚ¢4¢b±Lv&›ÑÝÙ°3›´B%/ZP
+ê!JOEë¡'ü?Ø½Ô‹
+­ZðàEÁ·‰;ðíÛ÷Í{ïûf˜®D·ÁþÖÊk.¬-Ö¦‰2•­S–!¾­6¶ÄZÚŒVÝk®Û³<ûðûT¤”¼^xÞ¤oÖÇëûÛC•rà8®Ç€åsÅó"ˆå0¡ÀWÜæj2®ÝF—‘2úðÛoè]4O„É¨IUž!€É™SY—–;LWd¸å{\X€r8V'4µrž›ç”I®Ã€PŠÊ® 6(×µe¹›²´oYA/‘P`¶m@*‹R9b^ ƒ´Ïm*!Ï<‰­P9/¸PY¾D+KÙêP¨L<
+ÒO›®ƒv¨\ã“Êö\asÁþ·€ÄIÛxä­„xf6Þ:dºžbÃ:iÛ_wìx¯ÞbÄtläÀèK¦ve£2ê¹è@XÑµYåú›¸ƒ’§|A™gs3e¡SdágPvx[V©œlF…‚áæ˜Ð&³†ëYÑ*›§MMžB(¬7a*lEFš.‚\5ržªÔ4b^…q¾™íˆ}ˆõ7åýî±y¸tïuüÐ»R¢vvâ÷·t¾{EN6ÆOßè‰_½õ82:Yƒu[‚YšVl@bo\ÓÆ¼V¼Œ¸˜A|Eh«+Ždßj‘ZkE"ÐCÑÅuK^à5­S«Ú='Šg{v~ütmtðóD,¿¡Ãº›[.½¹tåÙÉ¡–:ÑÿåzÇÌÞ÷ùŸS‰ó¥0ìÿeýxtçÕscgôÙéê•¦ù§/çë—zžLw¾½¿Ìÿ
